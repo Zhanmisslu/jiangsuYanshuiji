ed3baeb7a06102ea5784c9bbab710b5d05a9e715
ZDLLLL <1127733673@qq.com>
2018-12-19 13:34:35 +0800

    View下拉

--- app/src/main/java/com/example/zhan/heathmanage/Main/TrendFragment/TrendFragment.java
+++ app/src/main/java/com/example/zhan/heathmanage/Main/TrendFragment/TrendFragment.java
@@ -1,6 +1,7 @@
 package com.example.zhan.heathmanage.Main.TrendFragment;
 
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
@@ -8,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.zhan.heathmanage.R;
@@ -20,6 +23,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 /**
@@ -32,46 +36,44 @@ public class TrendFragment extends Fragment {
         // Required empty public constructor
     }
 
-    @BindView(R.id.week_tv) TextView week_tv;
-    @BindView(R.id.month_tv) TextView month_tv;
-    @BindView(R.id.year_tv) TextView year_tv;
-    @BindView(R.id.lunar_year_tv) TextView lunar_year_tv;
-    @BindView(R.id.lunar_day_tv) TextView lunar_day_tv;
-    @BindView(R.id.back_today_bt)
-    Button back_today_bt;
-
+//    @BindView(R.id.week_tv) TextView week_tv;
+//    @BindView(R.id.month_tv) TextView month_tv;
+//    @BindView(R.id.year_tv) TextView year_tv;
+//    @BindView(R.id.lunar_year_tv) TextView lunar_year_tv;
+//    @BindView(R.id.lunar_day_tv) TextView lunar_day_tv;
+//    @BindView(R.id.back_today_bt)
+//    Button back_today_bt;
+//    @BindView(R.id.fragment_evaluate_down_ib)
+//    ImageButton fragment_evaluate_down_ib;
+    @BindView(R.id.fragment_evaluate_ib)
+    ImageButton fragment_evaluate_ib;
+    @BindView(R.id.fragment_evaluate_view_ll)LinearLayout fragment_evaluate_view_ll;
+    @BindView(R.id.fragment_evaluate_ll)LinearLayout fragment_evaluate_ll;
+    int flag=0;
     private final String[] weeks = {"周一", "周二", "周三", "周四", "周五", "周六", "周日",};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_trend, container, false);
         ButterKnife.bind(this,view);
-        List<String> pointList = Arrays.asList("2018-10-01", "2018-11-19", "2018-11-20", "2018-05-23", "2019-01-01");
-        final Miui10Calendar miui10Calendar = view.findViewById(R.id.miui10Calendar);
-        miui10Calendar.setPointList(pointList);
-        back_today_bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                miui10Calendar.toToday();
-            }
-        });
-        miui10Calendar.setOnCalendarChangedListener(new OnCalendarChangedListener() {
-            @Override
-            public void onCalendarDateChanged(NDate date) {
-                year_tv.setText(date.localDate.getYear() + "年");
-                month_tv.setText(date.localDate.getMonthOfYear() + "月");
-                week_tv.setText(weeks[date.localDate.getDayOfWeek() - 1]);
-                lunar_year_tv.setText("农历" + date.lunar.lunarYearStr + "年 ");
-                lunar_day_tv.setText(date.lunar.lunarMonthStr + date.lunar.lunarDayStr + (TextUtils.isEmpty(date.lunarHoliday) ? "" : (" | " + date.lunarHoliday)) + (TextUtils.isEmpty(date.solarHoliday) ? "" : (" | " + date.solarHoliday)));
 
-            }
-
-            @Override
-            public void onCalendarStateChanged(boolean isMonthSate) {
-
-            }
-        });
         return view;
     }
+    @OnClick(R.id.fragment_evaluate_ll)
+    public void fragment_evaluate_ll_Onclick(){
+        if(flag==0){//0的时候是向上的箭头
+            flag=1;
+            fragment_evaluate_ib.setBackgroundResource(R.drawable.down);
+            fragment_evaluate_view_ll.setVisibility(View.VISIBLE);
+        }
+        else{//1的时候是向下的箭头
+            flag=0;
+            fragment_evaluate_ib.setBackgroundResource(R.drawable.upon);
+            fragment_evaluate_view_ll.setVisibility(View.GONE);
+        }
+
+
+    }
+
 
 }
